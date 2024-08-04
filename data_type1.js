58f58451e1953f54fa3d502b9910e409a0b6ab7b
Atharva Pachunde <138585875+atharvapachunde@users.noreply.github.com>
2024-08-04 22:53:29 +0530

    Add files via upload

---
+++ data_type1.js
@@ -0,0 +1,31 @@
+// var x = "Hi peeps" // dynamic datatype
+// console.log(typeof x)
+
+// ctrl + / on selected comments 
+
+// var x = 0.7
+// console.log( typeof x)
+
+// var b = true
+// console.log(typeof b)
+
+// var a = Boolean("True")
+// console.log(typeof a)
+
+// var i =  null 
+// console.log(typeof i)
+
+// var y
+// console.log(typeof y)
+
+// var x1
+// let x2
+// const x3 = 2 // need to initialize, or else wont work
+// x4 = 12
+
+// var x = 100;
+// var x = "SHS"  // using var we can declare multiple times
+
+//using let, you can declare only once 
+let z = 122
+// let z ="SHS"
