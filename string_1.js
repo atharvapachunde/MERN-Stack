913db40987e38d169a537532d404969f850592f9
Atharva Pachunde <138585875+atharvapachunde@users.noreply.github.com>
2024-08-07 23:19:30 +0530

    Add files via upload

---
+++ string_1.js
@@ -0,0 +1,27 @@
+var s1 = "Kalki"
+var s2 = 'Kalki'
+// var s3 = """Kalki""" //wont work
+// var s4 = '''Kalki''' // wont work
+
+var s3 = `Kalki`
+
+// string class in immutable class 
+
+// objects are nameless 
+// b1 is reference to object
+
+var b1 = "Pune"
+console.log(b1)
+
+b1 = b1 + " is"
+
+console.log(b1)
+
+// Sting interpolation
+
+let firstName = "Atharva";
+let lastName = "Pachunde";
+
+let text = `Welcome ${firstName} ${lastName}! `
+
+console.log(text)
