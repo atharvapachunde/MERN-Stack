564db30e87db2ff6acadd2cf991d884f2d9ae92a
Atharva Pachunde <138585875+atharvapachunde@users.noreply.github.com>
2024-08-09 22:47:48 +0530

    Add files via upload

---
+++ string_interpolation.js
@@ -0,0 +1,21 @@
+let firstName = "Atharva"
+let lastName = "Pachunde"
+
+let text1 = `My name is ${firstName} ${lastName}`
+
+let city = "Pune"
+
+let text2 =  `I live in ${city}`
+
+let course = "Btech in CSE"
+
+let text3 = `I am pursuing ${course}`
+
+let age = 19
+
+let text4 = `I am ${age} years old`
+
+console.log(text1)
+console.log(text2)
+console.log(text3)
+console.log(text4)
